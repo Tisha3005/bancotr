99f059bb7a8a68096efa526c7a0ae94bc33af42d
Tisha3005 <ribeiro_thais@hotmail.com>
2022-06-30 16:48:37 -0300

    Banco TR
    Criacao do Projeto Banco TR

---
+++ src/com/java/Conta.java
@@ -0,0 +1,7 @@
+package com.java;
+
+public class Conta {
+	int numero;
+	double saldo;
+	Cliente cliente = new Cliente();
+}
---
+++ src/com/java/TesteConta.java
@@ -0,0 +1,32 @@
+package com.java;
+
+public class TesteConta {
+
+	public static void main(String[] args) {
+		
+		Conta contaCorrente = new Conta();
+		Conta contaPoupanca = new Conta();
+		Conta contaInvestimento = new Conta();
+		
+		contaCorrente.numero = 11;
+		contaCorrente.saldo = 20.5;
+		contaCorrente.cliente.nome = "Tisha";
+		contaCorrente.cliente.idade = 45;
+		
+		contaPoupanca.numero = 22;
+		contaPoupanca.saldo = 30;
+		contaPoupanca.cliente.nome = "Sofia";
+		contaPoupanca.cliente.idade = 12;
+		
+		contaInvestimento.numero = 34;
+		contaInvestimento.saldo = 65000;
+		contaInvestimento.cliente.nome = "Pedro Henrique";
+		contaInvestimento.cliente.idade = 17;
+		
+		System.out.println("Numero da conta: " + contaInvestimento.numero);
+		System.out.println(contaInvestimento.saldo);
+		System.out.println(contaInvestimento.cliente.nome);
+		System.out.println(contaInvestimento.cliente.idade);
+		
+	}
+}
